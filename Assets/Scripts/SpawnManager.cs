42c42f1f67141efefc671792a516151be021ac8c
TF2026 <tfleitas26@sandomenico.org>
2023-10-23 12:28:21 -0700

    2.2.7 complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float spawnrange;
-    public float startDelay = 2.0f;
-    public float spawnInterval = 1.5f;
-
-    private GameObject[] animals;
+    private float spawnrange = 15f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+    [SerializeField] GameObject[] animals;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // spawns random animal
-    void SpawnRandomAnimal()
+    private void SpawnRandomAnimal()
     {
-        
+        int animalIndex = Random.Range(0, animals.Length);
+        float x = Random.Range(-spawnrange, spawnrange);
+        Vector3 spawnPosition = new Vector3(x, 0.0f, 25.0f);
+        GameObject animal = animals[animalIndex];
+
+        Instantiate(animal, spawnPosition, animal.transform.rotation);
     }
 }
